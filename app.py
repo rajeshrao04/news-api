d256eb645ac88591c5ab2ccd9a00a965b7ebbc16
rajeshrao04 <rajeshraoponugoti@gmail.com>
2017-06-13 11:34:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from flask import make_response
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['GET'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
     
@@ -38,36 +38,49 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "news.search":
         return {}
-    baseurl = "https://newsapi.org/v1/articles?source=the-times-of-india&sortBy=top&apiKey=dda1592b3267447193fb1756b5746b0e"
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+
     result = urlopen(baseurl).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
     return res
+
+
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+    
 
 def makeWebhookResult(data):
     query = data.get('query')
     if query is None:
         return {}
     
-    result = query.get('articles')
+    result = query.get('results')
     if result is None:
         return {}
     
-    channel = result.get('author')
+    channel = result.get('channel')
     if channel is None:
         return {}
     
-    item = channel.get('title')
-    location = channel.get('description')
-    units = channel.get('url')
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
     if (location is None) or (item is None) or (units is None):
         return {}
-
+    
+    condition = item.get('condition')
+    if condition is None:
+        return {}
 
     # print(json.dumps(item, indent=4))
-    
-    
-    speech = " latest news"+channel+"item"+""+location+""+"units"
+
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+        ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
 print("Response:")
     print(speech)
