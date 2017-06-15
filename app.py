9747f091117f1722aa15a44aea9d53c0116cd9ad
rajeshrao04 <rajeshraoponugoti@gmail.com>
2017-06-15 12:44:45 +0530

    Create app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") = "news.search":
+    if req.get("result").get("action")! = "news.search":
         return {}
     baseurl = "https://newsapi.org/v1/articles?source=the-times-of-india&sortBy=top&apiKey=60969da0a38e4cf1aad619158c413030"
     if yql_query is None:
@@ -78,7 +78,7 @@ def makeWebhookResult(res):
 
     print(json.dumps(item, indent=4))
 
-    speech = "latest news" + author.get() + " "+ title.get() +" "+ description.get() +" " + url.get()
+    speech = "latest news" + author.get() + "" + title.get() + "" + description.get() + "" + url.get()
 
     #print("Response:")
     print(speech)
